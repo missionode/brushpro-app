b05594d234d77722c68e1cc5ac1421fde773cb43
bodhiintellect <nath.syam.1986@gmail.com>
2025-09-30 15:03:25 +0530

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,9 @@
   document.getElementById("privacy-link").addEventListener("click", function(e) {
     e.preventDefault()
-    window.location.href = "shared/privacy_policy.html"
+    window.location.href = "/brushpro-app/shared/privacy_policy.html"
   })
 
   document.getElementById("terms-link").addEventListener("click", function(e) {
     e.preventDefault()
-    window.location.href = "shared/terms_of_service.html"
+    window.location.href = "/brushpro-app/shared/terms_of_service.html"
   })
